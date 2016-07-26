dd9fcada0315729dc50f3b15a79b7ad0cc045840
Bryan Baker <bryanpbaker@gmail.com>
2016-07-26 10:57:46 -0500

    added icons to home page and header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
 	return(
 		<div className="header col-xs-12">
 			<div className="navbar navbar-dark bg-danger">
-				<Link to="/" className="navbar-brand">Movie App</Link>
+				<Link to="/" className="navbar-brand"><i className="fa fa-ticket"></i> Movie App</Link>
 				<ul className="navbar-nav nav pull-right">
 					<li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
 					<li className="nav-item"><Link className="nav-link" to="/search">Search</Link></li>
